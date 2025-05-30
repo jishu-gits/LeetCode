3182d97293a7f9f0d15ca44067d90438e08f1556
Bidhan Manna <123752190+jishu-gits@users.noreply.github.com>
2025-05-30 21:26:55 +0530

    Time: 240 ms (41.25%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1801-average-time-of-process-per-machine/average-time-of-process-per-machine.sql
@@ -0,0 +1,8 @@
+SELECT StartActivity.machine_id, 
+       ROUND(AVG(EndActivity.timestamp - StartActivity.timestamp), 3) AS processing_time
+FROM Activity AS StartActivity
+INNER JOIN Activity AS EndActivity 
+USING (machine_id, process_id)
+WHERE StartActivity.activity_type = 'start' 
+AND EndActivity.activity_type = 'end'
+GROUP BY StartActivity.machine_id;
